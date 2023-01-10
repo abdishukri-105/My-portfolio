c5e38cf0d1ad189f336bb6507d4c2d0084e99eb9
abdishukri <abdishukrimohamed105@gmail.com>
2023-01-10 15:00:14 +0300

    added social links and buttons

--- js/index.js
+++ js/index.js
@@ -1,16 +1,23 @@
-// function calculateAge(age){
-  // return age * 6;
-//};
 
-//calculateAge();
-// arrow functions 
-// const old  = (x,y) =>{
-//     return x * y;
-// };
 
-function calculateAge(age, x = 4){
-    return age * x;
 
-}
+// Get the animated text element
+var animatedText = document.querySelector('.about');
 
-console.log(calculateAge(6));
+// Get the top position of the animated text element
+var animatedTextPosition = animatedText.getBoundingClientRect().top;
+
+// Get the window height
+var windowHeight = window.innerHeight;
+
+// Listen for scroll events
+window.addEventListener('scroll', function() {
+  
+  // Get the current scroll position
+  var scrollPosition = window.pageYOffset;
+  
+  // Check if the animated text element is within the viewport
+  if (scrollPosition > animatedTextPosition - windowHeight + 100) {
+    animatedText.classList.add('visible');
+  }
+});
